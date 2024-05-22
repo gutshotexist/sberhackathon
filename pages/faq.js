65b7c37b068a0703745c28270b44f32f465a2808
gutshot <lukoshkinanarchy@gmail.com>
2024-05-22 10:27:55 +0300

    fix casing

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../components/navbar";
+import NavBar from "../components/NavBar";
 import Faqcomponent from "../components/faq";
 
 const faq = () => {
